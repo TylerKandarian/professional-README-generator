f78afd50140b24a381638e9fb38e66fed1cc789c
tylerkandarian <tyler.kand2@gmail.com>
2022-10-31 14:59:29 -0400

    created array of questions

--- index.js
+++ index.js
@@ -46,3 +46,93 @@ const questions = () => {
             }
         }
     },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+                return false; 
+            }
+        }
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU', 'Apache', 'Mozilla-Public'],
+        default: ["MIT"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please choose a license!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter steps required to install your project!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this application?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a usage description!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'How do you test this project?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing to the repo?'
+    }
+]);
+};
+
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
+
+questions() 
+.then(answers => {
+    return generatePage(answers);
+})
+.then(data => {
+    return writeFile(data);
+})
+.catch(err => {
+    console.log(err)
+})
