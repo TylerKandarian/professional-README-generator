de169ec04ac8a94a5939ff8473e1ed8ee5cc0c20
tylerkandarian <tyler.kand2@gmail.com>
2022-10-31 14:24:29 -0400

    added first 3 questions to ask user

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 const inquirer = ("inquirer");
 const fs = require("fs");
-const generateMardown = require("")
+const generateMarkdown = require("./utils/generateMarkdown")
+
+//array of questions
+const questions = () => {
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false; 
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false; 
+            }
+        }
+    },
